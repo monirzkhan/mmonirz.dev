0e3151b2ad380a36248d4119187a1ec6f89137da
monirzkhan <odvutkabbo@gmail.com>
2026-06-08 15:58:19 +0600

    basic structure implemented

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section >
+        
+        <div>
+          <h1 className="text-4xl font-bold font-bricolage">MMonirz.Dev</h1>
+          <p className="text-lg mt-2 font-raleway">Welcome to my portfolio website! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and skills to see how I can contribute to your next big idea.</p>
+          <p className="text-lg mt-2 font-google">Welcome to my portfolio website! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and skills to see how I can contribute to your next big idea.</p>
+          <p className="text-lg mt-2 font-bricolage">Welcome to my portfolio website! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and skills to see how I can contribute to your next big idea.</p>
+         
+        </div>
+      
+      </section>
+
+      <div className="ticks"></div>
+
+      
+
+     
+    </>
+  )
+}
+
+export default App
