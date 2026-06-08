937915e33db85a6a3c881082ccc1cc135ca0cb2c
monirzkhan <odvutkabbo@gmail.com>
2026-06-08 19:12:41 +0600

    Navbar and footer design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './App.css'
+import Card from './Components/Banner/Card'
 
 function App() {
 
@@ -8,10 +9,16 @@ function App() {
   return (
     <>
       <section >
-        
+        <div className="h-96">
+          <Card></Card>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+            <h1 className="text-4xl font-bold text-white font-google">Hi, I'm Monir</h1>
+            <p className="text-lg mt-2 text-white font-google">Welcome to my portfolio website! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and skills to see how I can contribute to your next big idea.</p>
+          </div>
+        </div>
         <div>
-          <h1 className="text-4xl font-bold font-bricolage">MMonirz.Dev</h1>
-          <p className="text-lg mt-2 font-raleway">Welcome to my portfolio website! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and skills to see how I can contribute to your next big idea.</p>
+          
+          
           <p className="text-lg mt-2 font-google">Welcome to my portfolio website! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and skills to see how I can contribute to your next big idea.</p>
           <p className="text-lg mt-2 font-bricolage">Welcome to my portfolio website! I'm a passionate developer with a love for creating innovative solutions. Explore my projects and skills to see how I can contribute to your next big idea.</p>
          
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createRoot } from 'react-dom/client'
+import Layout from './Components/Layout/Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <Layout/>,
+    errorElement: <div>404 Not Found</div>,
+    children: [
+      {
+        index: true,
+        element: <Home/>,
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
